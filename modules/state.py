b5723183f1e0e7804126637a8324b0ff0fd5d9fc
Ansh Shukla <shuklaansh96@gmail.com>
2020-07-20 15:28:45 -0700

    Documentation

--- modules/state.py
+++ modules/state.py
@@ -1,13 +1,31 @@
+"""
+Object Classes written as part of Assignment 1
+State, Grid, and Position used for representing the
+8-Puzzle Problem
+
+Objects in this file are imported and used by
+puzzle_solver.py
+
+Author: Ansh Shukla [V00816280]
+Class: ECE 470
+Date: July 20, 2020
+"""
+
+
 from typing import List
 
 empty_space = ' '
-top_i = [0, 1, 2]
-left_i = [0, 3, 6]
-bottom_i = [6, 7, 8]
-right_i = [2, 5, 8]
+top_indices = [0, 1, 2]
+left_indices = [0, 3, 6]
+bottom_indices = [6, 7, 8]
+right_indices = [2, 5, 8]
 
 
 class Position:
+    """
+    Position defines a piece's co-ordinates in the 8-Puzzle Problem
+    This is used for calculating the Manhattan Distance Heuristic
+    """
     def __init__(self, x, y, value):
         self.x = x
         self.y = y
@@ -15,8 +33,12 @@ class Position:
 
 
 class Grid:
-
+    """
+    Grid representation of a state. Initializes all the pieces as positions in a grid
+    Used for calculating the Manhattan Distance between tiles
+    """
     def __init__(self, pieces: list):
+        assert len(pieces) == 9
         self.positions: List[Position] = []
         self.positions.append(Position(0, 0, pieces[0]))
         self.positions.append(Position(1, 0, pieces[1]))
@@ -28,19 +50,33 @@ class Grid:
         self.positions.append(Position(1, 2, pieces[7]))
         self.positions.append(Position(2, 2, pieces[8]))
 
-    def get_pos(self, value):
+    def get_pos(self, value) -> Position:
+        """
+        Returns the position of a piece that contains the value specified
+        :param value: The value of the piece whose position is being retrieved
+        :return: Position
+        """
         for position in self.positions:
             if position.value == value:
                 return position
 
 
 class State:
-    #  pieces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    #  board =
-    #   [0][1][2]
-    #   [3][4][5]
-    #   [6][7][8]
+    """
+    Object representing a state of the 8-Piece Problem
+    """
     def __init__(self, pieces: list):
+        """
+        Takes in an array of pieces and initializes the state
+
+        If the pieces passed in are [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        They are represented as:
+        [0][1][2]
+        [3][4][5]
+        [6][7][8]
+
+        :param pieces: Pieces in order of configuration
+        """
         assert len(pieces) == 9
         inf = float('inf')
         self.parent_state: State = None
@@ -50,17 +86,38 @@ class State:
         self.g_score = inf
         self.f = inf
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
+        """
+        Tells python how to properly compare two instances of a State
+        :param other: The object being compared to this instance
+        :return: True if the objects are equivalent, false otherwise
+        """
         if not isinstance(other, type(self)):
             raise ValueError(f"Object is of type {type(other)}. Expected type {type(self)}")
         return self.board == other.board
 
-    def empty_piece_idx(self):
+    def generate_grid(self) -> Grid:
+        """
+        Generates a Grid instance of this State instance
+        :return: The generated Grid
+        """
+        return Grid(self.board.copy())
+
+    def empty_piece_idx(self) -> int:
+        """
+        Finds the index of the empty space in the board
+        Used to determine possible moves for the empty space
+        :return: The index of the empty space
+        """
         for i, piece in enumerate(self.board):
             if piece == empty_space:
                 return i
 
-    def stringify(self):
+    def stringify(self) -> str:
+        """
+        Creates a human readable representation of the State instance
+        :return: The string representation of the State instance
+        """
         state = ''
         for i, piece in enumerate(self.board):
             if i > 0 and i % 3 == 0:
@@ -69,35 +126,48 @@ class State:
         return state
 
     def print_state(self):
+        """
+        Prints the state in a human readable representation
+        """
         print(self.stringify())
 
-    def swap_pieces(self, x, y):
-        temp = self.board[x]
-        self.board[x] = self.board[y]
-        self.board[y] = temp
+    def swap_pieces(self, x_idx, y_idx):
+        """
+        Swaps pieces in the board
+        Used for moving around a piece (The empty piece)
+        :param x_idx: The index of piece x
+        :param y_idx: The index of piece y
+        """
+        temp = self.board[x_idx]
+        self.board[x_idx] = self.board[y_idx]
+        self.board[y_idx] = temp
 
     def possible_moves(self):
+        """
+        Determines the possible moves for the empty space
+        :return: A list of possible states
+        """
         states: List[State] = []
-        empty_i = self.empty_piece_idx()
+        empty_idx = self.empty_piece_idx()
         # Move blank_piece left
-        if empty_i not in left_i:
+        if empty_idx not in left_indices:
             left_state = State(self.board.copy())
-            left_state.swap_pieces(empty_i, empty_i - 1)
+            left_state.swap_pieces(empty_idx, empty_idx - 1)
             states.append(left_state)
         # Move blank_piece up
-        if empty_i not in top_i:
+        if empty_idx not in top_indices:
             up_state = State(self.board.copy())
-            up_state.swap_pieces(empty_i, empty_i - 3)
+            up_state.swap_pieces(empty_idx, empty_idx - 3)
             states.append(up_state)
         # Move blank_piece right
-        if empty_i not in right_i:
+        if empty_idx not in right_indices:
             right_state = State(self.board.copy())
-            right_state.swap_pieces(empty_i, empty_i + 1)
+            right_state.swap_pieces(empty_idx, empty_idx + 1)
             states.append(right_state)
         # Move blank_piece down
-        if empty_i not in bottom_i:
+        if empty_idx not in bottom_indices:
             down_state = State(self.board.copy())
-            down_state.swap_pieces(empty_i, empty_i + 3)
+            down_state.swap_pieces(empty_idx, empty_idx + 3)
             states.append(down_state)
 
         return states
--- puzzle_solver.py
+++ puzzle_solver.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+8-Puzzle Problem Solver written as part of Assignment 1
+Uses A* and two separate heuristic functions to find the goal state
+
+Author: Ansh Shukla [V00816280]
+Class: ECE 470
+Date: July 20, 2020
+"""
+
 from modules.state import State, Grid, empty_space, List
 
 
-def h1(current_state: State):
+def h1(current_state: State) -> int:
+    """
+    This admissible heuristic function calculates the
+    number of misplaces tiles in the current state
+    :param current_state: The state to compare against the goal state
+    :return: h_score
+    """
     # h_score_1 = number of misplaced tiles
     h_score = 0
     for state_piece, goal_piece in zip(current_state.board, goal_state.board):
@@ -12,17 +29,29 @@ def h1(current_state: State):
     return h_score
 
 
-def h2(current_state: State):
+def h2(current_state: State) -> int:
+    """
+    This admissible heuristic function calculates the
+    total Manhattan distance between misplaced tiles
+    :param current_state: The state to compare against the goal state
+    :return: h_score
+    """
     # h_score_2 = Manhattan Distance between misplaced nodes
     h_score = 0
-    curr_grid = Grid(current_state.board.copy())
+    curr_grid: Grid = current_state.generate_grid()
     for goal_position in goal_grid.positions:
         curr_pos = curr_grid.get_pos(goal_position.value)
         h_score += (abs(curr_pos.x - goal_position.x) + abs(curr_pos.y - goal_position.y))
     return h_score
 
 
-def reconstruct_path(end_state: State):
+def reconstruct_path(end_state: State) -> List[State]:
+    """
+    Reconstructs the path taken from the start state to the goal state
+    by going back and recording state's parents until the start state
+    :param end_state: The state to start reconstructing from
+    :return: The list of states progressed in order
+    """
     path: List[State] = []
     curr_state = end_state
     while curr_state.parent_state is not None:
@@ -34,6 +63,9 @@ def reconstruct_path(end_state: State):
 
 
 def print_initial_states():
+    """
+    Convenience function to print out goal and start states
+    """
     print("Goal State:")
     goal_state.print_state()
     print("\nStart State:")
@@ -41,12 +73,14 @@ def print_initial_states():
     print()
 
 
-def a_star(h):
+def a_star(h) -> List[State]:
     """
     A Star Algorithm implemented via pseudo-code found at:
     https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
 
-    :param h: Heuristic Function to use
+    as well as email hints from Professor Deepali Arora (UVic)
+
+    :param h: Heuristic Function to use to calculate h_score
     :return: A path list to goal state
     """
     g_score = 0
@@ -67,13 +101,16 @@ def a_star(h):
         print("Expanded States:")
         for expanded_state in current.possible_moves():
             if expanded_state not in closed_list:
+                # Add explored state to closed list to reduce analyzing again
                 closed_list.append(expanded_state)
+                # If g_score to path better than its current recorded g_score
                 if g_score < expanded_state.g_score:
+                    # Record g_score
                     expanded_state.parent_state = current
                     expanded_state.g_score = g_score
                     expanded_state.h_score = h(expanded_state)
+                    # Calculated f = g + h
                     expanded_state.f = expanded_state.g_score + expanded_state.h_score
-
                     expanded_state.print_state()
                     print(f'g={expanded_state.g_score} h={expanded_state.h_score} f={expanded_state.f}\n')
 
@@ -85,14 +122,14 @@ def a_star(h):
 
 if __name__ == '__main__':
     goal_state = State([1, 4, 7, 2, 5, 8, 3, 6, empty_space])
-    goal_grid = Grid(goal_state.board.copy())
+    goal_grid: Grid = goal_state.generate_grid()
 
     # Assignment input
     # start_state = State([7, 4, 5, 2, empty_space, 6, 8, 3, 1])
     # Test Input 1
-    start_state = State([1, 4, 7, 2, 5, 8, empty_space, 3, 6])
+    # start_state = State([1, 4, 7, 2, 5, 8, empty_space, 3, 6])
     # Test Input 2
-    # start_state = State([4, empty_space, 7, 1, 5, 8, 2, 3, 6])
+    start_state = State([4, empty_space, 7, 1, 5, 8, 2, 3, 6])
     # Test Input 3
     # start_state = State([4, 7, 6, 1, empty_space, 5, 2, 8, 3])
 
@@ -111,20 +148,3 @@ if __name__ == '__main__':
     for state in h1_path:
         state.print_state()
         print('')
-
-    # states = start_state.possible_moves()
-    # for state in states:
-    #     state.f = h1(state)
-    #     state.print_state()
-    #     print("f =", state.f)
-    #
-    # print("\nsorted")
-    # states.sort(key=lambda x: x.f, reverse=True)
-    # for state in states:
-    #     state.f = h1(state)
-    #     state.print_state()
-    #     print("f =", state.f)
-    #
-    # print("\npopped")
-    # current = states.pop()
-    # current.print_state()
