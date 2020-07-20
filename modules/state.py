aa4b34a7a257873c26339bbad8fe2f88835f92f6
Ansh Shukla <shuklaansh96@gmail.com>
2020-07-20 00:56:50 -0700

    A*

---
+++ modules/state.py
@@ -0,0 +1,76 @@
+from typing import List
+
+empty_space = ' '
+top_i = [0, 1, 2]
+left_i = [0, 3, 6]
+bottom_i = [6, 7, 8]
+right_i = [2, 5, 8]
+
+
+class State:
+    #  pieces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    #  board =
+    #   [0][1][2]
+    #   [3][4][5]
+    #   [6][7][8]
+    def __init__(self, pieces: list):
+        assert len(pieces) == 9
+        inf = float('inf')
+        self.parent_state: State = None
+        self.board = pieces
+        self.h_score = inf
+    # g = the number of nodes traversed from the start state to the current state
+        self.g_score = inf
+        self.f = inf
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            raise ValueError(f"Object is of type {type(other)}. Expected type {type(self)}")
+        return self.board == other.board
+
+    def empty_piece_idx(self):
+        for i, piece in enumerate(self.board):
+            if piece == empty_space:
+                return i
+
+    def stringify(self):
+        state = ''
+        for i, piece in enumerate(self.board):
+            if i > 0 and i % 3 == 0:
+                state += '\n'
+            state += f'[{piece}]'
+        return state
+
+    def print_state(self):
+        print(self.stringify())
+
+    def swap_pieces(self, x, y):
+        temp = self.board[x]
+        self.board[x] = self.board[y]
+        self.board[y] = temp
+
+    def possible_moves(self):
+        states: List[State] = []
+        empty_i = self.empty_piece_idx()
+        # Move blank_piece left
+        if empty_i not in left_i:
+            left_state = State(self.board.copy())
+            left_state.swap_pieces(empty_i, empty_i-1)
+            states.append(left_state)
+        # Move blank_piece up
+        if empty_i not in top_i:
+            up_state = State(self.board.copy())
+            up_state.swap_pieces(empty_i, empty_i-3)
+            states.append(up_state)
+        # Move blank_piece right
+        if empty_i not in right_i:
+            right_state = State(self.board.copy())
+            right_state.swap_pieces(empty_i, empty_i+1)
+            states.append(right_state)
+        # Move blank_piece down
+        if empty_i not in bottom_i:
+            down_state = State(self.board.copy())
+            down_state.swap_pieces(empty_i, empty_i+3)
+            states.append(down_state)
+
+        return states
---
+++ puzzle_solver.py
@@ -0,0 +1,77 @@
+from modules.state import State, empty_space, List
+
+
+def h1(current_state: State):
+    # h_score_1 = number of misplaced tiles
+    h_score = 0
+    for state_piece, goal_piece in zip(current_state.board, goal_state.board):
+        if state_piece == empty_space:
+            continue
+        if state_piece != goal_piece:
+            h_score += 1
+    return h_score
+
+
+def reconstruct_path(end_state: State):
+    path: List[State] = []
+    curr_state = end_state
+    while curr_state.parent_state is not None:
+        path.append(curr_state.parent_state)
+        curr_state = curr_state.parent_state
+    path.reverse()
+    path.append(goal_state)
+    return path
+
+
+def a_star(h):
+    g_score = 0
+    open_list: List[State] = [start_state]
+    while len(open_list) > 0:
+        g_score += 1
+        open_list.sort(key=lambda x: x.f, reverse=True)
+        current = open_list.pop()
+
+        if current == goal_state:
+            return reconstruct_path(current)
+
+        for expanded_state in current.possible_moves():
+            if g_score < expanded_state.g_score:
+                expanded_state.parent_state = current
+                expanded_state.g_score = g_score
+                expanded_state.h_score = h(expanded_state)
+                expanded_state.f = expanded_state.g_score + expanded_state.h_score
+                if expanded_state not in open_list:
+                    open_list.append(expanded_state)
+    return []
+
+
+if __name__ == '__main__':
+    goal_state = State([1, 4, 7, 2, 5, 8, 3, 6, empty_space])
+    # Assignment input
+    # start_state = State([7, 4, 5, 2, empty_space, 6, 8, 3, 1])
+    # Test Input 1
+    # start_state = State([1, 4, 7, 2, 5, 8, empty_space, 3, 6])
+    # Test Input 2
+    start_state = State([4, empty_space, 7, 1, 5, 8, 2, 3, 6])
+
+    h1_path = a_star(h1)
+    for state in h1_path:
+        state.print_state()
+        print('')
+
+    # states = start_state.possible_moves()
+    # for state in states:
+    #     state.f = h1(state)
+    #     state.print_state()
+    #     print("f =", state.f)
+    #
+    # print("\nsorted")
+    # states.sort(key=lambda x: x.f, reverse=True)
+    # for state in states:
+    #     state.f = h1(state)
+    #     state.print_state()
+    #     print("f =", state.f)
+    #
+    # print("\npopped")
+    # current = states.pop()
+    # current.print_state()
